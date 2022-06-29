9f651143afe73a10df48105e51f9a13f6f7bff01
arrazyfathan <arrazy.rabbani266@gmail.com>
2022-06-29 11:33:31 +0700

    update : add splashscreen

---
+++ app/src/main/java/com/arrazyfathan/rickmorty/ui/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.arrazyfathan.rickmorty.ui
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.arrazyfathan.rickmorty.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            Intent(this, MainActivity::class.java).also {
+                startActivity(it)
+            }
+            finish()
+        }, 800)
+    }
+}
